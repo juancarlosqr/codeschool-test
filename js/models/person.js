310134233c109489ea230f7669bb9e000afbaa42
Juan Carlos Quintero <jcquintero@wuelto.com.mx>
2014-11-13 15:42:25 -0430

    adding username in person model

--- js/models/person.js
+++ js/models/person.js
@@ -1,6 +1,7 @@
 App.Person = DS.Model.extend({
     firstName: DS.attr(),
     lastName: DS.attr(),
+    username: DS.attr(),
     email: DS.attr()
 });
 
@@ -11,30 +12,35 @@ App.Person.FIXTURES = [
         id: 1,
         firstName: 'Jhon',
         lastName: 'Doe',
-        email: 'jhon.doe@emberjscss.com'
+        email: 'jhon.doe@emberjscss.com',
+        username: 'jhon.doe'
     },
     {
         id: 2,
         firstName: 'Michael',
         lastName: 'Stewart',
-        email: 'michael.stewart@emberjscss.com'
+        email: 'michael.stewart@emberjscss.com',
+        username: 'michael.stewart'
     },
     {
         id: 3,
         firstName: 'Derek',
         lastName: 'Smith',
-        email: 'derek.smith@emberjscss.com'
+        email: 'derek.smith@emberjscss.com',
+        username: 'derek.smith'
     },
     {
         id: 4,
         firstName: 'Jane',
         lastName: 'Doe',
-        email: 'jane.doe@emberjscss.com'
+        email: 'jane.doe@emberjscss.com',
+        username: 'jane.doe'
     },
     {
         id: 5,
         firstName: 'Ryan',
         lastName: 'Dale',
-        email: 'ryan.dale@emberjscss.com'
+        email: 'ryan.dale@emberjscss.com',
+        username: 'ryan.dale'
     }
 ];
